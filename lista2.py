71fbfa8ec0073526f20a1481ef0418f51378e334
Andrzej Pietraszko <and.pietraszko@gmail.com>
2020-06-20 18:35:16 +0200

    Add loop

--- lista2.py
+++ lista2.py
@@ -1,3 +1,8 @@
 list_items = [["banan" , "marchew"] , ["mielonka", "schabowy"], [ "krawat" , "koszula" ]]
 list_shops = ("warzywniak", "mięsny", "odzieżowy")
 dict_shoping = dict(zip(list_shops, list_items))
+
+number_items = []
+for k , v in dict_shoping.items () :
+    print ("Ide do ", k.capitalize(), " aby kupić produkty : ",v )
+    number_items.append(len(v))
