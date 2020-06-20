c642772688cdb5495aa8015285b5090a166423d2
Andrzej Pietraszko <and.pietraszko@gmail.com>
2020-06-20 18:34:07 +0200

    Add lists

---
+++ lista2.py
@@ -0,0 +1,3 @@
+list_items = [["banan" , "marchew"] , ["mielonka", "schabowy"], [ "krawat" , "koszula" ]]
+list_shops = ("warzywniak", "mięsny", "odzieżowy")
+dict_shoping = dict(zip(list_shops, list_items))
